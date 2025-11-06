771f1fbb1d0f405ab189c632afff7339c72296be
ashutosh6603 <ashutosh6603@gmail.com>
2025-11-06 20:23:55 +0530

    created auth_service and connected postgreSQL db and also configured prisma configuration... To be continued from 8C

---
+++ services/auth-service/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = Number(process.env.port || 3001);
+
+  await app.listen(port, '0.0.0.0');
+  console.log(`Auth service listening on port ${port}`);
+}
+bootstrap();
